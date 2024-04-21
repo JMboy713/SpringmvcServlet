ebc068b65bbbe552fc9e01ce9a4c67a10837bf3e
Jmboy713 <jimin713@naver.com>
2024-04-21 14:57:05 +0900

    frontcontroller v1

---
+++ src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberFormControllerV1.java
@@ -0,0 +1,18 @@
+package hello.servlet.web.frontcontroller.v1.controller;
+
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberFormControllerV1 implements ControllerV1 {
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp"; // 이동할 경로
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);// controller -> view 이동
+        dispatcher.forward(request, response); // 호출시 servlet -> jsp 호출
+    }
+}
